8ac46d8c8c20841de2e61aeeb7bf8f6db206b22f
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-24 08:53:50 +0200

    Update Sidebar.jsx

--- e-commerce-advance-filtering-project/src/Sidebar/Sidebar.jsx
+++ e-commerce-advance-filtering-project/src/Sidebar/Sidebar.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
 import Category from "./Category/Category";
 import Price from "./Price/Price";
 import Colors from "./Colors/Colors";
-import './Sidebar.css'
+import "./Sidebar.css";
 
-const Sidebar = () => {
+const Sidebar = ({ handleChange }) => {
   return (
     <>
-        <section className='sidebar'>
-            <div className='logo-container'>
-                <h1>🛒</h1>
-            </div>
-        <Category  />
-        <Price  />
-        <Colors  />
-        </section>
+      <section className="sidebar">
+        <div className="logo-container">
+          <h1>🛒</h1>
+        </div>
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        <Colors handleChange={handleChange} />
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
