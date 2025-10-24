9710be669619f52f9e3785dbe85fd3e916ff9b4b
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-24 08:52:35 +0200

    Update Nav.jsx

--- e-commerce-advance-filtering-project/src/Navigation/Nav.jsx
+++ e-commerce-advance-filtering-project/src/Navigation/Nav.jsx
@@ -1,26 +1,32 @@
 import { FiHeart } from "react-icons/fi";
 import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
-import './Nav.css'
+import "./Nav.css";
 
-const Nav = () => {
+const Nav = ({ handleInputChange, query }) => {
   return (
     <nav>
       <div className="nav-container">
-        <input type="text" className="search-input" placeholder="Enter your shoe search" />
+        <input
+          className="search-input"
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          placeholder="Enter your search shoes."
+        />
       </div>
       <div className="profile-container">
         <a href="#">
           <FiHeart className="nav-icons" />
         </a>
-        <a href="#">
+        <a href="">
           <AiOutlineShoppingCart className="nav-icons" />
         </a>
-        <a href="#">
+        <a href="">
           <AiOutlineUserAdd className="nav-icons" />
         </a>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
