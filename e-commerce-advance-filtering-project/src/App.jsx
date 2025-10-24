a09a4dee72d155a5d514c9dd3e2071fc0602a894
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-24 08:26:40 +0200

    Sidebar section

--- e-commerce-advance-filtering-project/src/App.jsx
+++ e-commerce-advance-filtering-project/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Navigation from './Navigation/Nav'
 import Products from './Products/Products'
 import Recommended from './Recommended/Recommended'
+import Sidebar from './Sidebar/Sidebar'
 
 const App = () => {
   return (
     <div>
+      <Sidebar/>
       <Navigation />
       <Products />
       <Recommended />
