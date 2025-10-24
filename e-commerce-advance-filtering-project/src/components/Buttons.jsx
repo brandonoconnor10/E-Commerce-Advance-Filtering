b66b606f5dfc016a9ccebe90c85f320cb85fb98f
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-24 08:52:09 +0200

    Update Buttons.jsx

--- e-commerce-advance-filtering-project/src/components/Buttons.jsx
+++ e-commerce-advance-filtering-project/src/components/Buttons.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
 
-const Buttons = () => {
+const Buttons = ({ onClickHandler, value, title }) => {
   return (
-    <div>Buttons</div>
-  )
-}
+    <button onClick={onClickHandler} value={value} className="btns">
+      {title}
+    </button>
+  );
+};
 
-export default Buttons
+export default Buttons;
