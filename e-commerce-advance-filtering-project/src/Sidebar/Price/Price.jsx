faa0cf27fad90c4b318da231761995c8f41fdd96
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-24 08:53:47 +0200

    Update Price.jsx

--- e-commerce-advance-filtering-project/src/Sidebar/Price/Price.jsx
+++ e-commerce-advance-filtering-project/src/Sidebar/Price/Price.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import './Price.css'
+import Input from "../../components/Input";
+import "./Price.css";
 
-const Price = () => {
+const Price = ({ handleChange }) => {
   return (
-    <div>Price</div>
-  )
-}
+    <div className="ml">
+      <h2 className="sidebar-title price-title">Price</h2>
 
-export default Price
+      <label className="sidebar-label-container">
+        <input 
+          onChange={handleChange} 
+          type="radio" 
+          value="" 
+          name="price-filter" 
+        />
+        <span className="checkmark"></span>All
+      </label>
+
+      <Input
+        handleChange={handleChange}
+        value={50}
+        title="$0 - $50"
+        name="price-filter"
+      />
+
+      <Input
+        handleChange={handleChange}
+        value={100}
+        title="$50 - $100"
+        name="price-filter"
+      />
+
+      <Input
+        handleChange={handleChange}
+        value={150}
+        title="$100 - $150"
+        name="price-filter"
+      />
+
+      <Input
+        handleChange={handleChange}
+        value={200}
+        title="Over $150"
+        name="price-filter"
+      />
+    </div>
+  );
+};
+
+export default Price;
