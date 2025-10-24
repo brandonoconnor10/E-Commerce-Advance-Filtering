c698436c92e9e9e0e1cfb42f8e6d4600e559ee5b
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-24 08:21:56 +0200

    Styling and code for the recommended section

--- e-commerce-advance-filtering-project/src/Recommended/Recommended.jsx
+++ e-commerce-advance-filtering-project/src/Recommended/Recommended.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Button from '../components/Buttons'
 import './Recommended.css'
 
 const Recommended = () => {
   return (
-    <div>Recommended</div>
+    <>
+      <div>
+        <h2 className='recommended-title'>Recommended</h2>
+        <div className="recommended-flex">
+          <Button value="" title="All Products" />
+          <Button value="Nike" title="Nike" />
+          <Button value="Adidas" title="Adidas" />
+          <Button value="Puma" title="Puma" />
+          <Button value="Vans" title="Vans" />
+        </div>
+      </div>
+    </>
   )
 }
 
