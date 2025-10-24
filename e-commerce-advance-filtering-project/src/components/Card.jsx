ceeda753a039ed26012d151f143167a2bf37bb39
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-24 08:52:15 +0200

    Update Card.jsx

--- e-commerce-advance-filtering-project/src/components/Card.jsx
+++ e-commerce-advance-filtering-project/src/components/Card.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { BsFillBagFill } from "react-icons/bs";
+import { AiFillStar } from "react-icons/ai";
 
-const Card = () => {
+const Card = ({ img, title, rating, reviews, prevPrice, newPrice }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <section className="card">
+      <img src={img} alt={title} className="card-img" />
+      <div className="card-details">
+        <h3 className="card-title">{title}</h3>
 
-export default Card
+        <section className="card-reviews">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <AiFillStar key={i} className="rating-star" />
+            ))}
+          <span className="total-reviews">({reviews} reviews)</span>
+        </section>
+
+        <section className="card-price">
+          <div className="price">
+            <del>${prevPrice.toFixed(2)}</del> ${newPrice.toFixed(2)}
+          </div>
+          <div className="bag">
+            <BsFillBagFill className="bag-icon" />
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+};
+
+export default Card;
