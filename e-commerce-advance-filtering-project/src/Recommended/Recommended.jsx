ff2f8d95537354540005b25d5770f682b3f99258
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-24 08:52:44 +0200

    Update Recommended.jsx

--- e-commerce-advance-filtering-project/src/Recommended/Recommended.jsx
+++ e-commerce-advance-filtering-project/src/Recommended/Recommended.jsx
@@ -1,21 +1,21 @@
-import Button from '../components/Buttons'
-import './Recommended.css'
+import Button from "../components/Buttons";
+import "./Recommended.css";
 
-const Recommended = () => {
+const Recommended = ({ handleClick }) => {
   return (
     <>
       <div>
-        <h2 className='recommended-title'>Recommended</h2>
+        <h2 className="recommended-title">Recommended</h2>
         <div className="recommended-flex">
-          <Button value="" title="All Products" />
-          <Button value="Nike" title="Nike" />
-          <Button value="Adidas" title="Adidas" />
-          <Button value="Puma" title="Puma" />
-          <Button value="Vans" title="Vans" />
+          <Button onClickHandler={handleClick} value="" title="All Products" />
+          <Button onClickHandler={handleClick} value="Nike" title="Nike" />
+          <Button onClickHandler={handleClick} value="Adidas" title="Adidas" />
+          <Button onClickHandler={handleClick} value="Puma" title="Puma" />
+          <Button onClickHandler={handleClick} value="Vans" title="Vans" />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Recommended
+export default Recommended;
